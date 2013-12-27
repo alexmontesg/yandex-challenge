0366175ef9adc3f14854168d876c502b9931538a
Alejandro <alejandro.montes.g@gmail.com>
2013-12-27 17:39:07 +0100

    Added FeaturesToRankLib

---
+++ java/src/main/java/nl/tue/we/yandex/formatter/FeaturesToRankLib.java
@@ -0,0 +1,65 @@
+package nl.tue.we.yandex.formatter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import nl.tue.we.yandex.features.Feature;
+import nl.tue.we.yandex.features.LogAction;
+
+/**
+ * 
+ * @author Alejandro Montes Garcia
+ * 
+ */
+public class FeaturesToRankLib {
+
+	private List<String> knownFeatures;
+
+	public void format(Collection<LogAction> logActions, String outputFile)
+			throws IOException {
+		knownFeatures = new LinkedList<String>();
+		FileWriter fw = new FileWriter(outputFile, true);
+		for (LogAction logAction : logActions) {
+			fw.write(actionToLine(logAction));
+		}
+		fw.close();
+	}
+
+	private String actionToLine(LogAction logAction) {
+		Map<Integer, Float> featureMap = new HashMap<Integer, Float>();
+		for (Feature feature : logAction.getFeatures()) {
+			String featureName = feature.getName();
+			if (!knownFeatures.contains(featureName)) {
+				knownFeatures.add(featureName);
+			}
+			featureMap.put(knownFeatures.indexOf(featureName),
+					feature.getValue());
+		}
+		String line = buildLine(logAction.getLabel().getQueryId(), featureMap,
+				new TreeSet<Integer>(featureMap.keySet()));
+		return line;
+	}
+
+	private String buildLine(int queryId, Map<Integer, Float> featureMap,
+			SortedSet<Integer> orderedKeys) {
+		StringBuilder strb = new StringBuilder();
+		// TODO Append number in front
+		strb.append(" qid:");
+		strb.append(queryId);
+		for (Integer key : orderedKeys) {
+			strb.append(" ");
+			strb.append(key);
+			strb.append(":");
+			strb.append(featureMap.get(key));
+		}
+		strb.append("\n");
+		return strb.toString();
+	}
+}
